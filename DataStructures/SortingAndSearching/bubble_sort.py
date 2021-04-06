88dad12ba4ec7c1848f4a9ea058b26a6e3d460ae
ceballots <jordi299@gmail.com>
2021-04-06 21:03:18 +0200

    Add sorting and search

---
+++ DataStructures/SortingAndSearching/bubble_sort.py
@@ -0,0 +1,9 @@
+def bubble_sort(array):
+  swapped = True
+  while swapped:
+    swapped = False
+    for i in range(len(array) - 1):
+      if array[i] > array[i+1]:
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = True
+  return array
